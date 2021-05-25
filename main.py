e3c52cf871f5097d4845884b5071168f3c2471e9
piotrnowakowski <61783713+piotrnowakowski@users.noreply.github.com>
2021-05-26 01:16:43 +0200

    initial commit
    Added TODO for all main tasks.

---
+++ main.py
@@ -0,0 +1,44 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# TODO: 2 pola tekstowe planszy do wprowadzania rozmiarów
+# TODO: obsługa wyjątków gdy mniejsze od 2x2 lub większe od 15x15
+
+# TODO: plansza generowana na podstawie pól
+
+# TODO: liczba oznaczonych pól
+
+# TODO: wprowadzenie ile min na planszy
+
+# TODO: rozpoczęcie nowej gry
+
+# TODO: losowe rozmieszczenie min
+
+# TODO: kliknięcie w dane pole
+
+# TODO: kliknięcie w bombę
+
+# TODO: kliknięcie w puste pole
+# TODO: znikanie tam gdzie nie ma obok bomb
+# TODO: wyświetlanie liczby bomb przy polach wolnych na miejscach brzegowych
+
+# TODO: oznaczenie miny
+# TODO: drugie kliknięcie zmiana z tu jest na tu może
+# TODO: trzecie kliknięcie znika oznaczenie
+
+# TODO: zakończenie gry : oznaczenie wszystkich bomb, kliknięcie wszystkich pustych pól bez miny
+
+# TODO: cheat - kliknięcie x y Z Z y   ściemnienie wszystkich pól gdzie sa bomby
